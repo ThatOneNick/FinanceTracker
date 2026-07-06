ce65bb5d6153323b0a0bdd4fdde2c012bd773c98
Nick <nickmuch23@gmail.com>
2026-07-06 13:52:10 -0500

    Fixed build error.
    Removed blank window show functions.

--- FinanceTracker/MainWindow.xaml.cs
+++ FinanceTracker/MainWindow.xaml.cs
@@ -24,25 +24,5 @@ namespace FinanceTracker
             this.Close();
         }
 
-        private void btnBills_Click(object sender, RoutedEventArgs e)
-        {
-            BillsWindow billsWindow = new BillsWindow();
-            billsWindow.Show();
-            this.Close();
-        }
-
-        private void btnSubscriptions_Click(object sender, RoutedEventArgs e)
-        {
-            SubscriptionsWindow subscriptionsWindow = new SubscriptionsWindow();
-            subscriptionsWindow.Show();
-            this.Close();
-        }
-
-        private void btnOtherExpenses_Click(object sender, RoutedEventArgs e)
-        {
-            OtherExpensesWindow otherExpensesWindow = new OtherExpensesWindow();
-            otherExpensesWindow.Show();
-            this.Close();
-        }
     }
 }
